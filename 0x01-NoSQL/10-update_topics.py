16d23942dcb879025d91a02f18dec45832c2c661
Siphiwo10 <siphiwoj10@gmail.com>
2024-10-22 14:35:04 +0000

    school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""changes all topics of a school document"""
+"""Module"""
 
 
 def update_topics(mongo_collection, name, topics):
-    """ Update topics of the school document based on the name"""
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    """changes all topics of a school document based on the name"""
+    mongo_collection.update_many({"name":name}, {"$set": {"topics": topics}})
